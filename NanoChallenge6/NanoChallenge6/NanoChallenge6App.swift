a4902f82ca6ebbd8cf63fdae4ef9581c79461ae5
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-08-23 17:49:22 -0300

    Criação do projeto

---
+++ NanoChallenge6/NanoChallenge6/NanoChallenge6App.swift
@@ -0,0 +1,20 @@
+//
+//  NanoChallenge6App.swift
+//  NanoChallenge6
+//
+//  Created by Beatriz Sato on 23/08/21.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge6App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
